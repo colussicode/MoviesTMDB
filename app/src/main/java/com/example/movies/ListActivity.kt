22da4a5d07d234de55fbb8fedd6acc8a637e74b6
colussicode <colussidev@gmail.com>
2021-08-04 19:44:10 -0300

    searchview filter

--- app/src/main/java/com/example/movies/ListActivity.kt
+++ app/src/main/java/com/example/movies/ListActivity.kt
@@ -2,6 +2,7 @@ package com.example.movies
 
 import android.os.Bundle
 import android.view.Menu
+import android.widget.SearchView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
 import com.example.movies.adapter.MovieListAdapter
@@ -19,11 +20,49 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.get_movies)
         recyclerView = findViewById(R.id.rv_movies_list)
+
         getMyData()
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.search_bar, menu)
+        val searchView : SearchView = findViewById(R.id.search_bar)
+
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+               return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                val retrofitBuilder = Retrofit.Builder()
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .baseUrl(BASE_URL)
+                    .build()
+                    .create(moviesData::class.java)
+
+                val retrofitData = retrofitBuilder.searchMovies("39fd0a08c0cc7fd3041fc14605c22358", newText.toString())
+                retrofitData.enqueue(object: Callback<MovieResponse?>{
+                    override fun onResponse(
+                        call: Call<MovieResponse?>,
+                        response: Response<MovieResponse?>
+                    ) {
+                        response.body()?.let {
+                            val movieAdapter = MovieListAdapter(it.results)
+                            recyclerView.adapter = movieAdapter
+                            movieAdapter.notifyDataSetChanged()
+                        }
+                    }
+
+                    override fun onFailure(call: Call<MovieResponse?>, t: Throwable) {
+                        t.printStackTrace()
+                    }
+
+                })
+
+                return true
+            }
+
+        })
         return true
     }
 
