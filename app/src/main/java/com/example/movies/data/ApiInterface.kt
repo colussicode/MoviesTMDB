3d57137ad98316f1d4a132aebca2f10f616a7c86
colussicode <you@example.com>
2022-06-28 23:28:08 -0300

    testing git

--- app/src/main/java/com/example/movies/data/ApiInterface.kt
+++ app/src/main/java/com/example/movies/data/ApiInterface.kt
@@ -6,6 +6,8 @@ import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface MoviesData {
+
+    //testing cloud repo
     @GET("movie/now_playing")
     fun getData(@Query("api_key") apiKey: String): Call<MovieResponse>
 
